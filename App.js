eaa56a86c92249c724ad4cf1489c1eb6c287add4
Cristian Jose Sierra Cardenas <crijosicar@hotmail.com>
2018-02-25 15:37:06 -0500

    fix app.js

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import configureStore from './src/store/index';
 
 const { persistor, store } = configureStore();
 
-
 export default function App() {
   return <Root store={store} persistor={persistor} />;
 }
