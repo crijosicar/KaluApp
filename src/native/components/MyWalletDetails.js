8e6283eaf645d9b5f187c7f648d13f35383f7f0f
Cristian Sierra Cardenas <crijosicar@hotmail.com>
2018-05-15 00:55:07 -0500

    fix picker

--- src/native/components/MyWalletDetails.js
+++ src/native/components/MyWalletDetails.js
@@ -8,105 +8,136 @@ import Header from './Header';
 import Spacer from './Spacer';
 import { PieChart } from 'react-native-svg-charts';
 import Messages from './Messages';
-import { View, ListView, StyleSheet,Text } from 'react-native';
+import { View, ListView, StyleSheet, Text, Platform } from 'react-native';
+
 import Notch from '../containers/Notch';
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: 20,
-    },
-  });
+  container: {
+    flex: 1,
+    marginTop: 20,
+  },
+});
 
 class MyWalletDetails extends React.Component {
-    constructor() {
-        super();
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state = {
-          dataSource: ds.cloneWithRows(['row 1', 'row 2']),
-          monthIncoming: ""
-          
-        };
-        this.onValueChangeMonthIncoming = this.onValueChangeMonthIncoming.bind(this);
-        
-      }
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+      monthIncoming: "",
+      yearIncoming: ""
 
-      onValueChangeMonthIncoming = (value) => {
-       // this.props.getPieValues(this.props.member,{month:value, year:this.state.yearIncoming, tipoTransaccion:"INGRESO"});
-        this.setState({ monthIncoming: value });
-    }
-    render() {
-        const { error } = this.props;
-        var allItems = new Array();
-        const year = new Date().getFullYear();
-        allItems = ['2018','2017','2016','2015'];
-    
-        const colors = ["#600080", "#2556BA", "#26995F", "#99263D", "#7F9B0F", "#9E2807", "#C98704", "#33260E"];
-       
+    };
+    this.onValueChangeMonthIncoming = this.onValueChangeMonthIncoming.bind(this);
+    this.onValueChangeYearIncoming = this.onValueChangeYearIncoming.bind(this);
+  }
 
-            
-                  return (
-                    <Container>
-                    <Content padder>
-                    <Header
-                            title="Detalles de Movimientos"
-                            content="Información Financiera"/>
+  onValueChangeMonthIncoming = (value) => {
+    //this.props.getPieValues(this.props.member,{month:this.state.monthIncoming, year:value, tipoTransaccion:"INGRESO"});
+    this.setState({ monthIncoming: value });
+  }
 
-                        
-                        <Grid>
-                          <Col>
-                            <H3>Ingresos</H3>
-                          </Col>
-                        </Grid>
-                        <Grid>
-                          <Col>
-                            <View style={{
-                              flex:1,
-                              flexDirection:'row',
-                              alignItems:'center',
-                              justifyContent:'center'
-                            }}>
-                              <Picker
-                                mode="dropdown"
-                                iosHeader="Seleccione..."
-                                iosIcon={<Icon name="ios-arrow-down-outline" />}
-                                headerBackButtonText="< Volver"
-                                placeholder="Mes"
-                                selectedValue={this.state.monthIncoming}
-                                onValueChange={this.onValueChangeMonthIncoming}>
-                                
-                                  <Picker.Item label="Enero" value="1" />
-                                  <Picker.Item label="Febrero" value="2" />
-                                  <Picker.Item label="Marzo" value="3" />
-                                  <Picker.Item label="Abril" value="4" />
-                                  <Picker.Item label="Mayo" value="5" />
-                                  <Picker.Item label="Junio" value="6" />
-                                  <Picker.Item label="Julio" value="7" />
-                                  <Picker.Item label="Agosto" value="8" />
-                                  <Picker.Item label="Septiembre" value="9" />
-                                  <Picker.Item label="Octubre" value="10" />
-                                  <Picker.Item label="Noviembre" value="11" />
-                                  <Picker.Item label="Diciembre" value="12" />
-                              </Picker>
-                            </View>
-                        </Col>
-                    </Grid>
-                    <Grid>
-                        <Col>
-                            <ListView
-                            style={styles.container}
-                            dataSource={this.state.dataSource}
-                            renderRow={(data) => <View><Text>{data}</Text></View>}
-                                />
-                        </Col>
-                    </Grid>                   
-                    </Content>
-                    <Notch></Notch>
-                </Container>
-        )
-    }
+  onValueChangeYearIncoming = (value) => {
+    //this.props.getPieValues(this.props.member,{month:this.state.monthIncoming, year:value, tipoTransaccion:"INGRESO"});
+    this.setState({ yearIncoming: value });
+  }
 
-    
+  render() {
+    const { error } = this.props;
+    var allItems = new Array();
+    const year = new Date().getFullYear();
+    allItems = ['2018','2017','2016','2015'];
+
+    const colors = ["#600080", "#2556BA", "#26995F", "#99263D", "#7F9B0F", "#9E2807", "#C98704", "#33260E"];
+
+    return (
+      <Container>
+        <Content padder>
+          <Header
+            title="Detalles de Movimientos"
+            content="Información Financiera"/>
+          <Grid>
+            <Col>
+              <H3>Ingresos</H3>
+            </Col>
+          </Grid>
+          <Grid>
+            <Col>
+              <View style={{
+                  flex:1,
+                  flexDirection:'row',
+                  alignItems:'center',
+                  justifyContent:'center'
+                }}>
+              <Picker
+                  mode="dropdown"
+                  iosHeader="Seleccione..."
+                  iosIcon={<Icon name="ios-arrow-down-outline" />}
+                  headerBackButtonText="< Volver"
+                  placeholder="Mes"
+                  placeholderStyle={{ color: "#2874F0" }}
+                  note={false}
+                  style={{ width:(Platform.OS === 'ios') ? undefined : 120 }}
+                  selectedValue={this.state.monthIncoming}
+                  onValueChange={this.onValueChangeMonthIncoming}>
+                    <Picker.Item label="Enero" value="1" />
+                    <Picker.Item label="Febrero" value="2" />
+                    <Picker.Item label="Marzo" value="3" />
+                    <Picker.Item label="Abril" value="4" />
+                    <Picker.Item label="Mayo" value="5" />
+                    <Picker.Item label="Junio" value="6" />
+                    <Picker.Item label="Julio" value="7" />
+                    <Picker.Item label="Agosto" value="8" />
+                    <Picker.Item label="Septiembre" value="9" />
+                    <Picker.Item label="Octubre" value="10" />
+                    <Picker.Item label="Noviembre" value="11" />
+                    <Picker.Item label="Diciembre" value="12" />
+                </Picker>
+              </View>
+            </Col>
+            <Col>
+              <View style={{
+                flex:1,
+                flexDirection:'row',
+                alignItems:'center',
+                justifyContent:'center'
+              }}>
+              <Picker
+                  mode="dropdown"
+                  iosHeader="Seleccione..."
+                  iosIcon={<Icon name="ios-arrow-down-outline" />}
+                  headerBackButtonText="< Volver"
+                  placeholder="Año"
+                  placeholderStyle={{ color: "#2874F0" }}
+                  note={false}
+                  style={{ width:(Platform.OS === 'ios') ? undefined : 120 }}
+                  selectedValue={this.state.yearIncoming}
+                  onValueChange={this.onValueChangeYearIncoming}>
+                        {allItems && allItems.map((anho,i) => {
+                          return (
+                            <Picker.Item key={i} label={anho} value={anho} />
+                        )})}
+              </Picker>
+            </View>
+            </Col>
+          </Grid>
+          <Grid>
+            <Col>
+              <ListView
+                style={styles.container}
+                dataSource={this.state.dataSource}
+                renderRow={(data) => <View><Text>{data}</Text></View>}
+                />
+            </Col>
+          </Grid>
+        </Content>
+        <Notch></Notch>
+      </Container>
+    )
+  }
+
+
 
 }
 
